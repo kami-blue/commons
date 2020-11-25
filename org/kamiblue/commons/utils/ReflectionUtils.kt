f68356259fc9f773acff4d3566d2d46de5b3f38a
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-11-24 21:32:17 -0500

    Added comments to ReflectionUtils

--- org/kamiblue/commons/utils/ReflectionUtils.kt
+++ org/kamiblue/commons/utils/ReflectionUtils.kt
@@ -9,10 +9,16 @@ object ReflectionUtils {
     private val defaultFunctions = Any::class.java.methods.toHashSet()
     private val classRegex = ".class$".toRegex()
 
+    /**
+     * Optimized version of [getSubclassOf], ignores inner classes and anonymous classes
+     */
     inline fun <reified T> getSubclassOfFast(packageName: String) = getSubclassOf<T>(packageName) {
         !it.name.contains('$')
     }
 
+    /**
+     * Get all subclasses of [T] under [packageName], filtered by [filter]
+     */
     inline fun <reified T> getSubclassOf(packageName: String, crossinline filter: (File) -> Boolean = { true }): List<Class<out T>> {
         val classList = ArrayList<Class<out T>>()
         val typeClass = T::class.java
@@ -35,6 +41,9 @@ object ReflectionUtils {
         return classList
     }
 
+    /**
+     * Get all top level functions under [packageName]
+     */
     fun getTopLevelFunctions(packageName: String): List<Method> {
         val functionList = ArrayList<Method>()
 
@@ -49,12 +58,18 @@ object ReflectionUtils {
         return functionList
     }
 
+    /**
+     * Convert the [packageName] to list of URL paths
+     */
     fun getPathsForPackage(packageName: String): List<String> =
         Thread.currentThread().contextClassLoader
             .getResources(packageName.replace('.', '/'))
             .toList()
             .map { it.file }
 
+    /**
+     * List all [File] including [File] under subdirectories, filtered by [filter]
+     */
     fun File.listAllFiles(filter: (File) -> Boolean): List<File> {
         val list = ArrayList<File>()
         this.listFiles()?.forEach {
@@ -64,6 +79,9 @@ object ReflectionUtils {
         return list
     }
 
+    /**
+     * Get the class for this [File], or null if the this [File] isn't a class
+     */
     fun File.toClass(packageName: String) = try {
         val relativePath = this.path
             .replace('\\', '.')
@@ -75,6 +93,11 @@ object ReflectionUtils {
         null
     }
 
+    /**
+     * Get the INSTANCE field of this class or singleton
+     *
+     * @throws NoSuchFieldException if this class does not has INSTANCE field or not a singleton
+     */
     inline val <reified T> Class<out T>.instance : T get() = this.getDeclaredField("INSTANCE")[null] as T
 
 }
