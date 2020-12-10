13514233844bf422fa10e2838568af8f8bb2c6ed
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-12-10 18:45:37 -0500

    [feature] Added sumByFloat extension function

--- org/kamiblue/commons/extension/Collection.kt
+++ org/kamiblue/commons/extension/Collection.kt
@@ -3,3 +3,14 @@ package org.kamiblue.commons.extension
 fun <E: Any> MutableCollection<E>.add(e: E?) {
     if (e != null) this.add(e)
 }
+
+/**
+ * Returns the sum of all values produced by [selector] function applied to each element in the collection.
+ */
+inline fun <T> Iterable<T>.sumByFloat(selector: (T) -> Float): Float {
+    var sum = 0.0f
+    for (element in this) {
+        sum += selector(element)
+    }
+    return sum
+}
