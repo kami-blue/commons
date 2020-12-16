5b7946d0e46a96ec867b54404c751142d2fd6ec9
Dominika <sokolov.dominika@gmail.com>
2020-12-15 23:08:39 -0500

    [fix] Clarify JavaDoc in String.kt

--- org/kamiblue/commons/extension/String.kt
+++ org/kamiblue/commons/extension/String.kt
@@ -1,6 +1,7 @@
 package org.kamiblue.commons.extension
 
 /**
+ * Eg, "house".max(4) will return "hous"
  * @return [this] with the length limited to [max]
  */
 fun String.max(max: Int) = this.substring(0, this.length.coerceAtMost(max))
