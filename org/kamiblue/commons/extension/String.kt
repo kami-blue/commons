3c425397afdb54fab53cf90724cb8f854602fbb7
Dominika <sokolov.dominika@gmail.com>
2020-12-24 11:10:21 -0500

    [feature] Added String.max(max, suffix)

--- org/kamiblue/commons/extension/String.kt
+++ org/kamiblue/commons/extension/String.kt
@@ -6,6 +6,18 @@ package org.kamiblue.commons.extension
  */
 fun String.max(max: Int) = this.substring(0, this.length.coerceAtMost(max))
 
+/**
+ * Eg, "house".max(4, "error") will return "erro"
+ * @return [this.max] with a message appended
+ */
+fun String.max(max: Int, suffix: String): String {
+    return if (length > max) {
+        (max(max - suffix.length) + suffix).max(max)
+    } else {
+        max(max)
+    }
+}
+
 fun String.surroundedBy(prefix: CharSequence, suffix: CharSequence, ignoreCase: Boolean = false) =
     this.startsWith(prefix, ignoreCase) && this.endsWith(suffix, ignoreCase)
 
