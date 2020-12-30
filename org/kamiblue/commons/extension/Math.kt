52c5c4f870e34c9d01dc983056b09bf4be50135c
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-12-30 05:26:02 -0500

    [new] Added toRadian() and toDegree()

--- org/kamiblue/commons/extension/Math.kt
+++ org/kamiblue/commons/extension/Math.kt
@@ -1,7 +1,10 @@
 package org.kamiblue.commons.extension
 
+import kotlin.math.PI
 import kotlin.math.ceil
 import kotlin.math.floor
+
+const val PI_FLOAT = 3.14159265358979323846f
 
 fun Double.floorToInt() = floor(this).toInt()
 
@@ -10,3 +13,11 @@ fun Float.floorToInt() = floor(this).toInt()
 fun Double.ceilToInt() = ceil(this).toInt()
 
 fun Float.ceilToInt() = ceil(this).toInt()
+
+fun Float.toRadian() = this / 180.0f * PI_FLOAT
+
+fun Double.toRadian() = this / 180.0 * PI
+
+fun Float.toDegree() = this * 180.0f / PI_FLOAT
+
+fun Double.toDegree() = this * 180.0 / PI
