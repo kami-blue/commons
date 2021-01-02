04dcb10f2c5405abfd9f2f42984126e077abae18
Dominika <sokolov.dominika@gmail.com>
2021-01-02 13:57:29 -0500

    [feature] Added CharArray.random(amount)

--- org/kamiblue/commons/extension/Array.kt
+++ org/kamiblue/commons/extension/Array.kt
@@ -1,21 +1,33 @@
 package org.kamiblue.commons.extension
 
-import java.util.*
+import java.util.Arrays
+import java.util.stream.DoubleStream
+import java.util.stream.IntStream
+import java.util.stream.LongStream
+import java.util.stream.Stream
+import kotlin.random.Random
 
-fun BooleanArray.stream() = Arrays.stream(this.toTypedArray())
+fun BooleanArray.stream(): Stream<Boolean> = Arrays.stream(this.toTypedArray())
 
-fun ByteArray.stream() = Arrays.stream(this.toTypedArray())
+fun ByteArray.stream(): Stream<Byte> = Arrays.stream(this.toTypedArray())
 
-fun ShortArray.stream() = Arrays.stream(this.toTypedArray())
+fun ShortArray.stream(): Stream<Short> = Arrays.stream(this.toTypedArray())
 
-fun IntArray.stream() = Arrays.stream(this)
+fun IntArray.stream(): IntStream = Arrays.stream(this)
 
-fun LongArray.stream() = Arrays.stream(this)
+fun LongArray.stream(): LongStream = Arrays.stream(this)
 
-fun FloatArray.stream() = Arrays.stream(this.toTypedArray())
+fun FloatArray.stream(): Stream<Float> = Arrays.stream(this.toTypedArray())
 
-fun DoubleArray.stream() = Arrays.stream(this)
+fun DoubleArray.stream(): DoubleStream = Arrays.stream(this)
 
-fun CharArray.stream() = Arrays.stream(this.toTypedArray())
+fun CharArray.stream(): Stream<Char> = Arrays.stream(this.toTypedArray())
 
-fun <T> Array<out T>.stream() = Arrays.stream(this)
+fun <T> Array<out T>.stream(): Stream<T> = Arrays.stream(this)
+
+fun CharArray.random(amount: Int) = (1..amount)
+    .map { Random.nextInt(0, this.size) }
+    .map(this::get)
+    .joinToString("")
+
+fun CharArray.remove(char: Char) = this.filter { it != char }.toCharArray()
