59e48f04dd32fd50627a63bd5987489ecebca36c
Xiaro <62033805+Xiaro@users.noreply.github.com>
2021-02-27 15:23:26 -0500

    Another attempt of class finder

--- org/kamiblue/commons/utils/ClassUtils.kt
+++ org/kamiblue/commons/utils/ClassUtils.kt
@@ -1,25 +1,93 @@
 package org.kamiblue.commons.utils
 
-import org.reflections.Reflections
+import java.io.File
+import java.net.URI
+import java.nio.file.Paths
+import java.util.*
+import java.util.function.Predicate
+import java.util.jar.JarInputStream
 
 object ClassUtils {
+    fun findClasses(
+        packageName: String,
+        predicate: Predicate<String> = Predicate { true }
+    ): List<Class<*>> {
+        val classLoader = Thread.currentThread().contextClassLoader
+        val packagePath = packageName.replace('.', '/')
 
-    inline fun <reified T> findClasses(
-        pack: String,
-        noinline block: Sequence<Class<out T>>.() -> Sequence<Class<out T>> = { this }
-    ): List<Class<out T>> {
-        return findClasses(pack, T::class.java, block)
+        val root = classLoader.getResource(packagePath)!!
+        val rootPath = root.toString().removeSuffix("!/$packagePath")
+        val rootURI = URI(rootPath)
+        val isJar = rootPath.startsWith("jar")
+
+        val classes = ArrayList<Class<*>>()
+
+        if (isJar) {
+            val file = Paths.get(URI(rootURI.schemeSpecificPart)).toFile()
+            findClassesInJar(classLoader, file, packagePath, predicate, classes)
+        } else {
+            classLoader.getResources(packagePath)?.let {
+                for (url in it) {
+                    findClasses(classLoader, File(url.file), packageName, predicate, classes)
+                }
+            }
+        }
+
+        return classes
     }
 
-    fun <T> findClasses(
-        pack: String,
-        subType: Class<T>,
-        block: Sequence<Class<out T>>.() -> Sequence<Class<out T>> = { this }
-    ): List<Class<out T>> {
-        return Reflections(pack).getSubTypesOf(subType).asSequence()
-            .run(block)
-            .sortedBy { it.simpleName }
-            .toList()
+    private fun findClasses(
+        classLoader: ClassLoader,
+        directory: File,
+        packageName: String,
+        predicate: Predicate<String> = Predicate { true },
+        list: MutableList<Class<*>>
+    ): List<Class<*>> {
+        if (!directory.exists()) return list
+
+        val packagePath = packageName.replace('.', File.separatorChar)
+
+        directory.walk()
+            .filter { it.isFile }
+            .filter { it.extension == "class" }
+            .map { it.path.substringAfter(packagePath) }
+            .map { it.replace(File.separatorChar, '.') }
+            .map { it.substring(0, it.length - 6) }
+            .map { "$packageName$it" }
+            .filter { predicate.test(it) }
+            .map { Class.forName(it, false, classLoader) }
+            .toCollection(list)
+
+        return list
+    }
+
+    private fun findClassesInJar(
+        classLoader: ClassLoader,
+        jarFile: File,
+        packageName: String,
+        predicate: Predicate<String> = Predicate { true },
+        list: MutableList<Class<*>>
+    ) {
+        JarInputStream(jarFile.inputStream()).use {
+            var entry = it.nextJarEntry
+            while (entry != null) {
+                if (!entry.isDirectory) {
+                    val name = entry.name
+
+                    if (name.startsWith(packageName) && name.endsWith(".class")) {
+                        val className = name
+                            .replace('/', '.')
+                            .substring(0, name.length - 6)
+
+                        if (predicate.test(className)) {
+                            list.add(Class.forName(className, false, classLoader))
+                        }
+                    }
+                }
+
+                entry = it.nextJarEntry
+            }
+        }
     }
 
     @Suppress("UNCHECKED_CAST")
